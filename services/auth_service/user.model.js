6b1c18d0acadfea3c4b7760acf67b4f6f06d4720
Nandun Bandara <ntbandara3@hotmail.com>
2017-05-16 07:59:26 +0530

    user model

--- services/auth_service/user.model.js
+++ services/auth_service/user.model.js
@@ -1,3 +1,14 @@
 /**
  * Created by ntban_000 on 5/15/2017.
  */
+const
+    mongoose    =   require('mongoose');
+    Schema      =   mongoose.Schema;
+
+module.exports = mongoose.model('User', new Schema({
+    name: String,
+    email: String,
+    password: String,
+    contact: String
+}))
+
