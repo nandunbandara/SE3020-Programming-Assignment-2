77490f6ec061aac7115f4461bf009a4cbb6bf5bf
Nandun Bandara <ntbandara3@hotmail.com>
2017-05-16 07:54:23 +0530

    auth service initialization

--- services/auth_service/config.js
+++ services/auth_service/config.js
@@ -1,3 +1,6 @@
 /**
  * Created by ntban_000 on 5/15/2017.
  */
+module.exports = {
+    'database': 'mongodb://auth_root:pass123@ds139791.mlab.com:39791/se3020_auth_service'
+}
--- services/auth_service/server.js
+++ services/auth_service/server.js
@@ -1,8 +1,19 @@
 /**
  * Created by ntban_000 on 5/15/2017.
  */
-const express = require('express')
-const app = express();
+const
+express     = require('express')
+app         = express();
+body_parser = require('body-parser');
+morgan      = require('morgan');
+mongoose    = require('mongoose');
+
+//json web tokens to validate users
+jwt         = require('jsonwebtoken');
+//application configurations
+config      = require('./config');
+//user schema
+User        = require('./user.model')
 
 app.listen(7001, function(){
     console.log('Authentication service server listening on 7001');
