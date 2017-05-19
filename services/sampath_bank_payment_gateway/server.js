e433e556326bfbcb399dd57bafe1e0eb08689837
Nandun Bandara <ntbandara3@hotmail.com>
2017-05-19 08:20:10 +0530

    sampath bank server implementation

--- services/sampath_bank_payment_gateway/server.js
+++ services/sampath_bank_payment_gateway/server.js
@@ -1,3 +1,24 @@
 /**
  * Created by ntban_000 on 5/19/2017.
  */
+const express = require('express')
+    mongoose = require('mongoose')
+    bodyParser = require('body-parser')
+    morgan = require('morgan')
+
+//app configurations
+app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//echo requests to console
+app.use(morgan('dev'));
+
+//listen
+app.listen(7008, err=>{
+    if(err){
+        console.log("Can not start service. Port already in use!");
+        return;
+    }
+    console.log("Sampath Bank Payment Gateway server listening on 7001")
+})
