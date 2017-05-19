1a6720883d8b1326a8888c588819060696830c84
Nandun Bandara <ntbandara3@hotmail.com>
2017-05-19 08:57:06 +0530

    payment schema implemented sbpg

--- services/sampath_bank_payment_gateway/payment.model.js
+++ services/sampath_bank_payment_gateway/payment.model.js
@@ -1,3 +1,14 @@
 /**
  * Created by ntban_000 on 5/19/2017.
  */
+const mongoose = require('mongoose');
+    Schema = mongoose.Schema;
+
+const PaymentSchema  = new Schema({
+    name: { type:String, required:true },
+    amount: { type:Number, required:true },
+    account_id: { type:String, required:true },
+    dateTime: { type:String, required:true }
+})
+
+module.exports = mongoose.model('Payment', PaymentSchema);
