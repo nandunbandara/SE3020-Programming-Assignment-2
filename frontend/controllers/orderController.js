f8a2a9e1bf591e59076ea651c1bb87c6f15b61df
root <ntbandara3@hotmail.com>
2017-06-25 21:51:40 +0530

    order payment method view

--- frontend/controllers/orderController.js
+++ frontend/controllers/orderController.js
@@ -4,5 +4,18 @@
 angular.module('cinema.orderController',[])
 
 .controller('orderCtrl', [function(){
+    var app = this;
 
+    //get selected no of seats
+    var count = $('#seats:selected').length;
+    //snack variables
+    app.popcorn = 0;
+    app.salt = 0;
+    app.cheese = 0;
+    app.chicken = 0;
+    app.coke = 0;
+    app.pepsi = 0;
+
+    app.total = (app.popcorn*250)+(app.salt*100)+(app.cheese*100)+(app.chicken*100)
+        +(app.coke*100)+(app.pepsi*100);
 }])
